7efc89e947a6c6f0d54e8ad84c44ad86ee3bcf52
Nazif Barassounon <nazifbarassounon@gmail.com>
2026-07-27 14:10:55 +0100

    replace the go version ci test with test runs

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,13 +11,11 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
-	}
+
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+	if len(splitAuth) < 2 || splitAuth[0] != "Api" {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], nil
+	return splitAuth[1] + "abcd", nil
 }
