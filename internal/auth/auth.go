f9cb0ed5b3c99b35178b22fb700e0ea9afd9cdbf
Nazif Barassounon <nazifbarassounon@gmail.com>
2026-07-27 14:16:44 +0100

    fix the GetAPIKey function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,11 +11,13 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-
+	if authHeader == "" {
+		return "", ErrNoAuthHeaderIncluded
+	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "Api" {
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1] + "abcd", nil
+	return splitAuth[1], nil
 }
